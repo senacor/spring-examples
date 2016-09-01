d5fe214e899dccfad24202a8cf3bb34b1d173598
Michael Menzel <michael.menzel@senacor.com>
2016-09-01 14:19:17 +0200

    Description in error handling example 2 changed

--- 2.error-handling/src/main/java/com/senacor/tecco/ilms/katas/example/e02_method/ErrorHandlingMethodController.java
+++ 2.error-handling/src/main/java/com/senacor/tecco/ilms/katas/example/e02_method/ErrorHandlingMethodController.java
@@ -4,24 +4,25 @@ import com.senacor.tecco.ilms.katas.common.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
+
 
 /**
  * Created by fsubasi on 05.01.2016.
  *
- * It's possible to add extra (@ExceptionHandler) methods to any controller to specifically
- * handle exceptions thrown by request handling (@RequestMapping) methods in the same controller
+ * Exception Example 2: Intercepting Exception in Controllers
+ *
+ * To handle exceptions thrown by request handling (@RequestMapping) methods,
+ * methods annotated with @ExceptionHandler can be added to any controller.
+ *
  * These (@ExceptionHandler) methods take precedence over other exception handlers (for example,
- * over global exception controllers)
+ * over exception handling performed in controller advices, see Example 3)
  *
  * In this example we throw an IllegalArgumentException and observe that it is intercepted
  * by the @ExceptionHandler annotated method in this controller.
  */
 
-@Controller
+@RestController
 @RequestMapping("/errorHandling/method")
 public class ErrorHandlingMethodController {
 
@@ -34,10 +35,11 @@ public class ErrorHandlingMethodController {
     // @ExceptionHandler declares an exception handler for the specified
     // exception types. Only exceptions thrown in the same controller
     // will be intercepted by the error handler method.
-    @ExceptionHandler(IllegalArgumentException.class)// NOTE TO SELF: If the annotation value is not used,
-    // then the exception types listed as method arguments are used
+    //
+    // If the annotation value is not used, then the exception types
+    // listed as method arguments are used
+    @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
-    @ResponseBody
     public ResponseEntity<Object> throwError(IllegalArgumentException e){
         return new ResponseEntity<Object>(createErrorMessage(e), HttpStatus.INTERNAL_SERVER_ERROR);
     }
