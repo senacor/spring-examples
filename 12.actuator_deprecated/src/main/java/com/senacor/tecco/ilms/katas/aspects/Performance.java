039bc52cd4b42aef88c50f0b4c456e9cbb75df44
Thomas Schnabel <thomas.schnabel@senacor.com>
2020-02-06 16:02:57 +0100

    remove actuator examples

--- 12.actuator_deprecated/src/main/java/com/senacor/tecco/ilms/katas/aspects/Performance.java
+++
@@ -1,5 +0,0 @@
-package com.senacor.tecco.ilms.katas.aspects;
-
-public interface Performance {
-    public void perform();
-}
--- 12.actuator_deprecated/src/main/java/com/senacor/tecco/ilms/katas/aspects/e02_introduction/Encoreable.java
+++
@@ -1,8 +0,0 @@
-package com.senacor.tecco.ilms.katas.aspects.e02_introduction;
-
-/**
- * Created by fsubasi on 06.01.2016.
- */
-public interface Encoreable {
-    void performEncore();
-}
